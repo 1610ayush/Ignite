ef57ed2702457290e674e9dd45f7d5f9416b5de2
1610ayush <ayushranjan.iit@gmail.com>
2021-09-13 16:57:42 +0530

    6. Final project

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -87,7 +87,7 @@ const GameDetail = ({ pathId }) => {
             <Media>
               <motion.img
                 layoutId={`image ${pathId}`}
-                src={smallImage(game.background_image, 1280)}
+                src={game.background_image}
                 alt={game.background_image}
               />
             </Media>
